1777c90e18d13cb4c096d4ba91e5aa30c6c7dd9d
Gadipally Sai Teja <teja.g14@gmail.com>
2020-02-06 10:02:36 -0500

    solution

--- Player.py
+++ Player.py
@@ -5,6 +5,7 @@ class Player:
     def __init__(self, cardset=None):
         self.sets = cardset
         self.hand = []
+        self.numDrawCards = 4
 
     def cardsLeft(self):
         if len(self.sets) != 0:
@@ -12,13 +13,18 @@ class Player:
         return False
 
     def drawCard(self):
-        card = self.sets.pop()
+        card = self.sets.pop(0)
         return card
 
     def addToSet(self, cards):
-        self.sets + cards
+        self.sets = self.sets + cards
+
+    def resetHand(self):
+        self.hand = []
 
     def startWar(self, card):
-        self.hand.extend([card] + self.sets[-2:])
-        self.sets = self.sets[:-2]
+        numCards = len(self.sets)
+        self.hand.extend([card] + self.sets[-1*self.numDrawCards:])
+        self.sets = self.sets[:-1*self.numDrawCards]
         self.sets.append(self.hand.pop())
+        return numCards
--- War.py
+++ War.py
@@ -7,21 +7,55 @@ class War:
         playerAcards, playerBcards = Cards().generateSets()
         self.playerA = Player(playerAcards)
         self.playerB = Player(playerBcards)
+        self.numDrawCards = self.playerA.numDrawCards
+        # self.playerA.sets = [(5,1)]
+        # self.playerB.sets = [(5, 0), (7, 2)]
 
     def start_game(self):
-        round =1
+        round = 1
+        print(self.playerA.sets[0][0])
+        print(self.playerB.sets[0][0])
         while self.playerA.cardsLeft() and self.playerB.cardsLeft():
+            print("XXXXXXXX round - " + str(round) + " XXXXXXXX")
             playerAcard = self.playerA.drawCard()
             playerBcard = self.playerB.drawCard()
-
+            print("player A draws " + str(playerAcard[0]))
+            print("player B draws " + str(playerBcard[0]))
             if playerAcard[0] == playerBcard[0]:
-                self.playerA.startWar(playerAcard)
-                self.playerB.startWar(playerBcard)
+                print("both cars are equal, war starts")
+                numA = self.playerA.startWar(playerAcard)
+                numB = self.playerB.startWar(playerBcard)
+                if numB < self.numDrawCards and numA < self.numDrawCards:
+                    print("game Draw")
+                    return
+                if numB < self.numDrawCards:
+                    print("Player B doesn't have enough cards to start war, Player A wins")
+                    return
+                if numA < self.numDrawCards:
+                    print("Player A doesn't have enough cards to start war, Player B wins")
+                    return
             elif playerAcard[0] > playerBcard[0]:
-
-                self.playerA.addToSet()
+                print("player B has lesser card, wins this round")
+                cards = self.playerA.hand + self.playerB.hand + [playerAcard, playerBcard]
+                self.playerA.addToSet(cards)
+                self.playerA.resetHand()
+                self.playerB.resetHand()
+            elif playerBcard[0] > playerAcard[0]:
+                print("player A has lesser card, wins this round")
+                cards = [playerAcard, playerBcard] + self.playerA.hand + self.playerB.hand
+                self.playerB.addToSet(cards)
+                self.playerA.resetHand()
+                self.playerB.resetHand()
+            # print(str(len(self.playerB.sets)))
+            print("Player A left with: "+str(len(self.playerA.sets))+" left")
+            print("Player B left with: " + str(len(self.playerB.sets)) + " left")
+            if len(self.playerB.sets) is 0:
+                print("player B wins")
+            if len(self.playerA.sets) is 0:
+                print("player A wins")
+            round += 1
 
 
 if __name__ == "__main__":
     game = War()
-    print(len(game.playerAcards))
+    game.start_game()
